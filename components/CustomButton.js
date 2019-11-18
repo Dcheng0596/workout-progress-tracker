021098f43b4c082a229c34ee344ac771c3ce07ce
Darien Cheng <dcheng0596@bths.edu>
2019-11-18 17:03:56 -0500

    Created Custom Button template

---
+++ components/CustomButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet} from 'react-native';
+
+const CustomButton = props => {
+    return (
+        <TouchableOpacity style={[styles.button, props.style]} onPress={props.onPress}>
+          <Text style={styles.text}>{props.title}</Text>
+        </TouchableOpacity>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 40
+    }
+  });
+
+  export default CustomButton;
