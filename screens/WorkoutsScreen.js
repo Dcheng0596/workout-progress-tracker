948014d5f88e92d46bd201f876939fa8596767e6
Darien Cheng <dcheng0596@bths.edu>
2019-11-23 13:07:15 -0500

    Removed createWorkoutButton

--- screens/WorkoutsScreen.js
+++ screens/WorkoutsScreen.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import WorkoutList from '../components/WorkoutList';
-import CustomButton from '../components/CustomButton';
 
 const WorkoutsScreen = ({ navigation }) => {
     return (
       <View style={styles.screen}>
         <WorkoutList navigation={navigation} />
-        <View style={styles.createWorkoutButton}>
-          <CustomButton title='+' onPress={() => navigation.navigate('CreateWorkoutModal')}/>
-        </View>
       </View>
     );
   };
@@ -17,14 +13,6 @@ const WorkoutsScreen = ({ navigation }) => {
   const styles = StyleSheet.create({
     screen: {
       flex: 1
-    },
-    createWorkoutButton: {
-      justifyContent: 'center',
-      backgroundColor: "#ffffff",
-      width: "100%",
-      height: "8%",
-      borderTopColor: 'black',
-      borderTopWidth: 1
     }
   });
 
