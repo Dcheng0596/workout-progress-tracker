b85417b29a749401746c48d3b17489e220eacaaa
Darien Cheng <dcheng0596@bths.edu>
2019-11-23 13:08:53 -0500

    Updated transition effects

--- stacks/RootStack.js
+++ stacks/RootStack.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
 import TabStack from './TabStack';
 import CreateWorkoutModal from '../modals/CreateWorkoutModal';
 import ViewWorkoutModal from '../modals/ViewWorkoutModal';
@@ -27,14 +27,18 @@ const RootStack = () => {
           name="ViewWorkoutModal" 
           component={ViewWorkoutModal}
           initialParams={{ isDone: false }}
-          options={({ navigation, route }) => ({ title: route.params.workout.name })}
+          options={({ navigation, route }) => ({ 
+            title: route.params.workout.name,
+            gestureEnabled: true ,
+            })}
         />
         <Stack.Screen 
           name="CreateWorkoutModal" 
           component={CreateWorkoutModal}
           initialParams={{ isDone: false }}
-          options={({ navigation }) => ({
+          options={({ navigation, tra }) => ({
               title: 'Create Workout',
+              ...TransitionPresets.ModalSlideFromBottomIOS,
               headerRight: () => (
                 <Button title="Done"  onPress={() => {
                   navigation.setParams({
